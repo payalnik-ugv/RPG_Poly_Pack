7f71bf061daf152f604929ce3086d296e7e4d0c5
payalnik <lazarchuklesyunichka@gmail.com>
2026-06-06 16:14:16 +0300

    #main

--- Assets/Diamond.cs
+++ Assets/Diamond.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Diamond : MonoBehaviour
 {
+    [SerializeField] private GameObject diamondVisual;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Diamond : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 3, 0);
+        diamondVisual.transform.Rotate(0, 3, 0);
     }
 }
